862acc754ba36c0256e75f16ca49c125b61883b3
Xiaoquan Kong <u1mail2me@gmail.com>
2018-05-04 18:41:37 +0800

    No more download english corpus since this project focus on Chinese language

--- download_wikipedia_raw_corpus.bash
+++ download_wikipedia_raw_corpus.bash
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-# download the latest English wikipedia dump
-wget -O raw_data/enwiki-latest-pages-articles.xml.bz2 -c "https://dumps.wikimedia.org/enwiki/latest/enwiki-latest-pages-articles.xml.bz2"
-
-# download the latest English wikipedia dump
+# download the latest Chinese wikipedia dump
 wget -O raw_data/zhwiki-latest-pages-articles.xml.bz2 -c "https://dumps.wikimedia.org/zhwiki/latest/zhwiki-latest-pages-articles.xml.bz2"
