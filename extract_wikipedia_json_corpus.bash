1418cc9f95b557d1025435d524b0bae2f0af16b1
Xiaoquan Kong <u1mail2me@gmail.com>
2018-05-10 15:02:05 +0800

    Using cpu count minus one processing worker

--- extract_wikipedia_json_corpus.bash
+++ extract_wikipedia_json_corpus.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-WikiExtractor.py --json raw_data/zhwiki-latest-pages-articles.xml.bz2 -o extracted_json_data --processes 8
+WikiExtractor.py --json raw_data/zhwiki-latest-pages-articles.xml.bz2 -o extracted_json_data
