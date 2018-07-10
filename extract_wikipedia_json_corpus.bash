644523eb675906d2cbdf453a3dff703d01b85800
Xiaoquan Kong <u1mail2me@gmail.com>
2018-07-10 23:46:50 +0800

    Add multiprocess wiki-extractor argument

--- extract_wikipedia_json_corpus.bash
+++ extract_wikipedia_json_corpus.bash
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-WikiExtractor.py --json raw_data/zhwiki-latest-pages-articles.xml.bz2 -o extracted_json_data
+cpu_count=`nproc --all`
+process_count=$(expr $cpu_count - 1)
+WikiExtractor.py --json raw_data/zhwiki-latest-pages-articles.xml.bz2 -o extracted_json_data --processes ${process_count}
