f136ffe910b4c45c687a566bd559e75ac9f7008c
Xiaoquan Kong <u1mail2me@gmail.com>
2020-03-09 15:30:36 +0800

    fix: python call file WikiExtractor.py

--- extract_wikipedia_json_corpus.bash
+++ extract_wikipedia_json_corpus.bash
@@ -2,4 +2,4 @@
 
 cpu_count=`nproc --all`
 process_count=$(expr $cpu_count - 1)
-WikiExtractor.py --json raw_data/zhwiki-latest-pages-articles.xml.bz2 -o extracted_json_data --processes ${process_count}
+python ./WikiExtractor.py --json raw_data/zhwiki-latest-pages-articles.xml.bz2 -o extracted_json_data --processes ${process_count}
