31670a53d67b214028b54611803e2df6e0bad3d3
Xiaoquan Kong <u1mail2me@gmail.com>
2020-03-09 15:31:53 +0800

    feat: show message when using cached wikipedia dumped file

--- download_wikipedia_raw_corpus.bash
+++ download_wikipedia_raw_corpus.bash
@@ -2,6 +2,8 @@
 
 # download the latest Chinese wikipedia dump
 file=raw_data/zhwiki-latest-pages-articles.xml.bz2
-if [ ! -f "$file" ]; then
+if [ -f "$file" ]; then
+    echo "Use cached wikipedia dump file"
+else
     wget -O ${file} -c "https://dumps.wikimedia.org/zhwiki/latest/zhwiki-latest-pages-articles.xml.bz2"
 fi
