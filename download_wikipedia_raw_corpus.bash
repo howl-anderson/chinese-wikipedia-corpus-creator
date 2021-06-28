07b75598d1429fb80a316b01bd290f3514db7b95
Xiaoquan Kong <u1mail2me@gmail.com>
2021-06-28 18:39:56 +0800

    update dependencies and modified code

--- download_wikipedia_raw_corpus.bash
+++ download_wikipedia_raw_corpus.bash
@@ -3,7 +3,7 @@
 # download the latest Chinese wikipedia dump
 file=raw_data/zhwiki-latest-pages-articles.xml.bz2
 if [ -f "$file" ]; then
-    echo "Use cached wikipedia dump file"
+    echo "Use cached wikipedia dump file ${file}"
 else
     wget -O ${file} -c "https://dumps.wikimedia.org/zhwiki/latest/zhwiki-latest-pages-articles.xml.bz2"
 fi
--- json_corpus_to_plain_text.py
+++ json_corpus_to_plain_text.py
@@ -1,10 +1,4 @@
 #!/bin/env python
-# -*- coding: utf-8 -*-
-
-from __future__ import division
-from __future__ import unicode_literals
-from __future__ import print_function
-from __future__ import absolute_import
 
 import sys
 import pathlib
@@ -13,6 +7,7 @@ import os
 
 from joblib import Parallel, delayed
 from tqdm import tqdm
+from bs4 import BeautifulSoup
 
 
 input_dir = sys.argv[1]
@@ -25,19 +20,20 @@ input_files = input_path.glob("**/wiki_*")
 
 def process_file(input_file):
     input_file_relative_name = input_file.relative_to(input_dir)
-    output_file_name = "-".join(input_file_relative_name.parts)
+    output_file_dir = output_dir / input_file_relative_name
+    output_file_dir.mkdir(parents=True, exist_ok=True)
 
-    with input_file.open('rt') as input_fd:
-        for index, line in enumerate(input_fd):
-            data_obj = json.loads(line)
+    with input_file.open("rt") as input_fd:
+        soup = BeautifulSoup(input_fd.read(), 'html.parser')
+        for index, line in enumerate(soup.find_all('doc')):
+            output_file = output_file_dir / str(index)
 
-            output_file = os.path.join(output_dir, output_file_name + "_" + str(index))
-            output_path = pathlib.Path(output_file)
+            plain_data = line.text
 
-            plain_data = data_obj['text']
-
-            with output_path.open('wt') as output_fd:
+            with output_file.open("wt") as output_fd:
                 output_fd.write(plain_data)
 
 
-Parallel(n_jobs=-1)(delayed(process_file)(input_file) for input_file in tqdm(input_files))
+Parallel(n_jobs=-1)(
+    delayed(process_file)(input_file) for input_file in tqdm(input_files)
+)
